3e98d3602390c3d0cdc05e6999b4ad0df8d19943
danislu <danislu@gmail.com>
2015-11-11 07:43:03 +0100

    no message

--- app/containers/mapViewContainer.js
+++ app/containers/mapViewContainer.js
@@ -1,8 +1,9 @@
 import { connect } from 'react-redux'
+
 import MapView from './../components/mapView';
 import Store from './../store';
 import * as actions from './../actions';
-import { PublishAction, SubscribeActions } from './../pubnub/backend';
+import { SubscribeActions, DispatchAndPublish } from './../pubnub/backend';
 
 const token = SubscribeActions((channel, action) => {
     Store.dispatch(action);
@@ -16,19 +17,14 @@ function mapStateToProps(state) {
     }
 }
 
-function mapDispatchToProps(innerDispatch) {
-    const dispatch = (action) => {
-        innerDispatch(action);
-        PublishAction(action);
-    }
-
+const mapDispatchToProps = DispatchAndPublish((dispatch) => {
     return {
         setZoom: (zoom) => dispatch(actions.setZoom(zoom)),
         setCenter: (x,y) => dispatch(actions.setCenter(x,y)),
         addPoint: (x,y) => dispatch(actions.addPoint(x,y)),
         removePoint: (index) => dispatch(actions.removePoint(index))
     }
-}
+});
 
 export default connect(
     mapStateToProps,
